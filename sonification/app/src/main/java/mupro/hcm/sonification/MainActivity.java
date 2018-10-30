fc51469bbc9fb6e1974668f4fc5b1052f6ee5687
David Heim <david.heim@student.uni-augsburg.de>
2018-10-30 11:52:56 +0100

    Base layout

--- sonification/app/src/main/java/mupro/hcm/sonification/MainActivity.java
+++ sonification/app/src/main/java/mupro/hcm/sonification/MainActivity.java
@@ -1,13 +1,54 @@
 package mupro.hcm.sonification;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import mupro.hcm.sonification.fragments.ChartsFragment;
+import mupro.hcm.sonification.fragments.HomeFragment;
+import mupro.hcm.sonification.fragments.MapFragment;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    switchFragment(new HomeFragment());
+                    return true;
+                case R.id.navigation_dashboard:
+                    switchFragment(new ChartsFragment());
+                    return true;
+                case R.id.navigation_notifications:
+                    switchFragment(new MapFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        switchFragment(new HomeFragment());
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
