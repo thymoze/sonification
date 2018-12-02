d36a3a51c6be7b47f968dda378d1dca6fd1bd053
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-12-02 14:21:51 +0100

    Added first version of working Google Maps fragment

--- sonification/app/src/main/java/mupro/hcm/sonification/fragments/MapFragment.java
+++ sonification/app/src/main/java/mupro/hcm/sonification/fragments/MapFragment.java
@@ -1,12 +1,37 @@
 package mupro.hcm.sonification.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.Polyline;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import mupro.hcm.sonification.R;
+import mupro.hcm.sonification.database.SensorData;
+import mupro.hcm.sonification.helpers.FusedLocationProvider;
+import mupro.hcm.sonification.helpers.GPSCoordinates;
+import mupro.hcm.sonification.helpers.JsonReceiver;
+
+import static mupro.hcm.sonification.MainActivity.BROADCAST_ACTION;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,20 +40,19 @@ import mupro.hcm.sonification.R;
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements
+        GoogleMap.OnMarkerClickListener,
+        OnMapReadyCallback {
+
+    SupportMapFragment mSupportMapFragment;
+    GoogleMap googleMap;
+
+    private final String TAG = "MapFragment";
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -37,13 +61,72 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mSupportMapFragment.getMapAsync(this);
+
+        JsonReceiver jsonReceiver = new JsonReceiver(this::updateMap);
+        IntentFilter intentFilter = new IntentFilter(BROADCAST_ACTION);
+        getContext().registerReceiver(jsonReceiver, intentFilter);
+
+        return v;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setOnMarkerClickListener(this);
+
+        FusedLocationProvider.requestSingleUpdate(getContext(), (location -> {
+            CameraPosition position = new CameraPosition.Builder()
+                    .target(new LatLng(location.latitude, location.longitude))
+                    .zoom(15.0f)
+                    .build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+        }));
+    }
+
+    public void addMarker(SensorData data) {
+        Marker marker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(data.getLatitude(), data.getLongitude()))
+                .title(data.getTimestamp()));
+        marker.setTag(data);
+
+        CameraPosition position = new CameraPosition.Builder()
+                .target(new LatLng(data.getLatitude(), data.getLongitude()))
+                .zoom(15.0f)
+                .build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        marker.setTag(clickCount);
+        Toast.makeText(getContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+
+        if (marker.getTag() != null)
+            Toast.makeText(getContext(), ((SensorData) marker.getTag()).getTimestamp(), Toast.LENGTH_LONG).show();
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+
+    }
+
+    private Void updateMap(JSONObject json) {
+        return null;
     }
 }
